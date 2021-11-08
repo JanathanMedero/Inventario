9d1e5614069c409dcfff60f37c4fe95ed970c741
JanathanMedero <medero.janathan@gmail.com>
2021-11-08 14:08:27 -0600

    employe table

--- app/Http/Livewire/ShowEmployees.php
+++ app/Http/Livewire/ShowEmployees.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
+use Auth;
+use App\Models\Role;
+use Livewire\WithPagination;
 
 class ShowEmployees extends Component
 {
+    use WithPagination;
+
+    public $search = "";
+
     public function render()
     {
-        return view('livewire.show-employees');
+        $employees = User::where('name', 'LIKE', '%' . $this->search . '%')->whereNotIn('id', [Auth::user()->id])->orderBy('created_at', 'DESC')->paginate(10);
+
+        $roles = Role::all();
+
+        return view('livewire.show-employees', compact('employees', 'roles'));
     }
 }
---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,7 @@
+<div class="col-md-12">
+	<div class="card card-stats mx-4 my-4 py-4">
+		<div class="card-body">
+			{{ $slot }}
+		</div>    
+	</div>
+</div>
